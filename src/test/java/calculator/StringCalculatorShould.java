fa44e6619c4274df1952b6d10644bc763730c3e1
Mayur Patil <mayurpatil5555mp@gamil.com>
2021-10-17 22:43:20 +0530

    StringCalculatorShould all functionality done

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -31,5 +31,41 @@ class StringCalculatorShould {
 	public void customDelimeter() throws Exception {
 		assertEquals(3, calculator.add("//;\n1;2"));
 	}
+	@Test
+    public void throwExceptionIfSingleNegativeNumber() throws Exception {
+        try {
+            calculator.add("//;\n1;-2;5");
+        }catch (StringCalculatorException e) {
+            assertEquals("negatives not allowed -2", e.getMessage());
+        }
+    }
+	@Test
+	public void throwExceptionIfMultipleNegativeNumber () throws RuntimeException  {
+		try {
+			calculator.add("-1,-2,3");
+		} catch(Exception e){
+			 assertEquals("negatives not allowed [-1, -2]", e.getMessage());
+		} 
+	}
+	@Test
+	public void validateCallCount(){
+		assertEquals(calculator.getCallCount(), calculator.count);
+	}
+	@Test
+	public void ignoreNumsGreaterThan1000() throws Exception {
+		assertEquals(2, calculator.add("2,1001"));
+	}
+	@Test
+	public void validateAnyLengthDelimeter() throws Exception {
+		assertEquals(6, calculator.add("//[***]\n1***2***3"));
+	}
+	@Test
+	public void multipleDelimeter() throws Exception	{
+		assertEquals(6, calculator.add("//[*][%]\n1*2%3"));
+	}
+	@Test
+	public void mulCustomDelWithAnyLength() throws Exception {
+		assertEquals(6, calculator.add("//[**][%%]\n1**2%%3"));
+	}
    
 }
