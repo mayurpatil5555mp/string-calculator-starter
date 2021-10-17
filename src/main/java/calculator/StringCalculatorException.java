c5fdc061430a7209868c21cf9ba74be8f39d91e6
Mayur Patil <mayurpatil5555mp@gamil.com>
2021-10-17 22:36:31 +0530

    Exception classes added

---
+++ src/main/java/calculator/StringCalculatorException.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringCalculatorException extends Exception {
+
+	private static final long serialVersionUID = 8009104046952508877L;
+
+	public StringCalculatorException(String message) {
+        super(message);
+    }
+}
